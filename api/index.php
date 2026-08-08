e27304851139d950fca843cd0ffdad1de86a3447
tjarot <aprilia.mutiara557@smk.belajar.id>
2026-08-08 14:18:28 +0700

    fix bootstrap cache path for vercel serverless

--- api/index.php
+++ api/index.php
@@ -1,10 +1,10 @@
 <?php
 
-// Buat folder temporary wajib di environment serverless Vercel
-$storageDirs = [
+// 1. Buat folder temporary yang wajib ada di serverless Vercel
+$dirs = [
     '/tmp/storage/run',
     '/tmp/storage/framework/views',
-    '/tmp/storage/framework/plugin',
+    '/tmp/storage/framework/plugins',
     '/tmp/storage/framework/sessions',
     '/tmp/storage/framework/cache',
     '/tmp/storage/framework/cache/data',
@@ -12,14 +12,21 @@ $storageDirs = [
     '/tmp/bootstrap/cache',
 ];
 
-foreach ($storageDirs as $dir) {
+foreach ($dirs as $dir) {
     if (!is_dir($dir)) {
         mkdir($dir, 0755, true);
     }
 }
 
-// Set path log dan cache ke /tmp
+// 2. Override path Laravel ke folder /tmp sebelum mengarahkan ke application
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
+putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
+putenv('APP_EVENTS_CACHE=/tmp/bootstrap/cache/events.php');
+
 $_ENV['APP_STORAGE_PATH'] = '/tmp/storage';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
 
+// 3. Jalankan aplikasi Laravel dari public/index.php
 require __DIR__ . '/../public/index.php';
