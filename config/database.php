fdb4f20e93f0a941639c173804fe04e0a2fc81fb
tjarot <aprilia.mutiara557@smk.belajar.id>
2026-08-08 14:40:51 +0700

    add ssl configuration for tidb cloud

--- config/database.php
+++ config/database.php
@@ -51,18 +51,16 @@ return [
     'database' => env('DB_DATABASE', 'forge'),
     'username' => env('DB_USERNAME', 'forge'),
     'password' => env('DB_PASSWORD', ''),
-    'unix_socket' => env('DB_SOCKET', ''),
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix' => '',
     'prefix_indexes' => true,
     'strict' => true,
     'engine' => null,
-    'options' => array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => base_path('isrgrootx1.pem'),
+    'options' => extension_loaded('pdo_mysql') ? array_filter([
+        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/certs/ca-certificates.crt'),
         PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-        PDO::MYSQL_ATTR_SSL_CAPATH => base_path(),
-    ]),
+    ]) : [],
 ],
 
 
