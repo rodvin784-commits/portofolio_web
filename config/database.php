67f466ce5c07cd3bffce2440950c5995780ec803
tjarot <aprilia.mutiara557@smk.belajar.id>
2026-08-08 14:47:34 +0700

    bypass ssl cert verify for tidb on vercel

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-    'mysql' => [
+   'mysql' => [
     'driver' => 'mysql',
     'url' => env('DATABASE_URL'),
     'host' => env('DB_HOST', '127.0.0.1'),
@@ -57,12 +57,11 @@ return [
     'prefix_indexes' => true,
     'strict' => true,
     'engine' => null,
-    'options' => extension_loaded('pdo_mysql') ? array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/certs/ca-certificates.crt'),
+    'options' => extension_loaded('pdo_mysql') ? [
+        PDO::MYSQL_ATTR_SSL_CA => NULL,
         PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-    ]) : [],
+    ] : [],
 ],
-
 
         'pgsql' => [
             'driver' => 'pgsql',
